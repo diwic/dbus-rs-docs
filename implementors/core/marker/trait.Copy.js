e671e260aa300cf4b25ed15c1dd8750741ca73ae
David Henningsson <david.henningsson@canonical.com>
2015-03-04 19:25:22 +0100

    Initial commit

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['dbus'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='dbus/enum.BusType.html' title='dbus::BusType'>DBusBusType</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='dbus/enum.NameFlag.html' title='dbus::NameFlag'>DBusNameFlag</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='dbus/enum.RequestNameReply.html' title='dbus::RequestNameReply'>DBusRequestNameReply</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='dbus/enum.ReleaseNameReply.html' title='dbus::ReleaseNameReply'>DBusReleaseNameReply</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='dbus/enum.MessageType.html' title='dbus::MessageType'>DBusMessageType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/sys/ext/trait.AsRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['dbus'] = ["<a class='stability Unstable' title='Unstable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/trait.AsRawFd.html' title='std::sys::ext::AsRawFd'>AsRawFd</a> for <a class='struct' href='dbus/struct.OwnedFd.html' title='dbus::OwnedFd'>OwnedFd</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
